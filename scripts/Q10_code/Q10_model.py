d6a905650c0ba24997a76de9332541b6f6978c05
FXXFERMI <fermi.fei@mail.utoronto.ca>
2024-03-21 03:58:29 -0400

    finish data plots

---
+++ scripts/Q10_code/Q10_model.py
@@ -0,0 +1,13 @@
+#### Preamble ####
+# Introduction:
+# Author: Siqi Fei
+# Date: 21 March 2024
+# Contact: fermi.fei@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: install pip pandas, numpy, random and sklearn
+
+import numpy as np
+from random import Random
+import pandas as pd
+import sklearn
+
