fb1937e40216c0c59ae377b95be6495232e6647b
FXXFERMI <fermi.fei@mail.utoronto.ca>
2024-04-07 03:30:49 -0400

    cleaned file structure

--- pred/new_model_logistic_newest.py
+++ pred/new_model_logistic_newest.py
@@ -10,14 +10,9 @@ import sys
 import csv
 import random
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import classification_report, accuracy_score, precision_score
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.metrics import accuracy_score, precision_score
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import LabelEncoder
 import data_cleaning as dc
 from sklearn.datasets import make_multilabel_classification
 from sklearn.linear_model import LogisticRegression
@@ -49,54 +44,8 @@ X_test_df = pd.concat([features_test_df, X_test_bow_df], axis=1)
 X_test = X_test_df.values
 
 
-# Using pandas to get descriptive statistics
-# print(X_train_bow_df.describe())
-# np.savetxt("/Users/fermis/Desktop/CSC311/CSC311_ML/data/pred_data/matrix/X_train.csv", X_train, delimiter=",", fmt='%i')
-
-# Save to CSV
-# train_final_dataset_path = '/Users/fermis/Desktop/CSC311/CSC311_ML/data/pred_data/train_final_dataset.csv'
-## Save coefficients to their own CSV file
-# X_train_df.to_csv(train_final_dataset_path, index=False)
-
-
-##### K-fold cross-validation ####
-# def create_k_folds(data, k=5):
-#    data_shuffled = data.sample(frac=1).reset_index(drop=True)  # Shuffle your dataset
-#    folds = np.array_split(data_shuffled, k)  # Split dataset into k folds
-#    return folds
-#
-#
-# k_folds = create_k_folds(clean_data, k=5)
-
-
-# def train_and_evaluate_k_fold(folds, model_init_func):
-#    acc_scores = []  # Store accuracy scores for each fold
-#
-#    for i in range(len(folds)):
-#        # Prepare training and validation data
-#        train_folds = [folds[j] for j in range(len(folds)) if j != i]
-#        train_data = pd.concat(train_folds)
-#        valid_data = folds[i]
-#
-#        # Prepare features and labels
-#        X_train = train_data.drop(['id', 'Label', 'Q10'], axis=1).values
-#        y_train = train_data['Label'].values
-#        X_valid = valid_data.drop(['id', 'Label', 'Q10'], axis=1).values
-#        y_valid = valid_data['Label'].values
-#
-#        # Initialize and train the model
-#        model = model_init_func()
-#        model.fit(X_train, y_train)
-#
-#        # Predict and evaluate
-#        predictions = model.predict(X_valid)
-#        accuracy = np.mean(predictions == y_valid)
-#        acc_scores.append(accuracy)
-#
-#        print(f"Fold {i + 1}: Accuracy = {accuracy}")
-#
-#    return np.mean(acc_scores), acc_scores  # Return the mean accuracy across all folds and scores per fold
-
+#### Model ####
+# References: CSC311 Winter2023-2024 lab9
 def softmax(z):
     exp_z = np.exp(z - np.max(z))
     return exp_z / exp_z.sum(axis=0)
@@ -105,7 +54,8 @@ def softmax(z):
 model = LogisticRegression(max_iter=2000, solver='lbfgs')
 multi_target_logreg = MultiOutputClassifier(model, n_jobs=-1)
 multi_target_logreg.fit(X_train, dc.t_train)
-# Make predictions
+
+###### Prediction ####
 train_pre = multi_target_logreg.predict(X_train)
 correct_train_predictions = np.all(train_pre == dc.t_train, axis=1)
 train_acc = np.mean(correct_train_predictions)
@@ -116,80 +66,29 @@ correct_predictions = np.all(val_pre == dc.t_valid, axis=1)
 val_acc = np.mean(correct_predictions)
 print("LR Valid Acc:", val_acc)
 
-estimators = []
-# Assuming multi_target_logreg is your fitted MultiOutputClassifier model
-for estimator in multi_target_logreg.estimators_:
+test_pre = multi_target_logreg.predict(X_test)
+correct_predictions = np.all(test_pre == dc.t_test, axis=1)
+test_acc = np.mean(correct_predictions)
+print("LR test Acc:", test_acc)
 
-    # Append the coefficients of the current estimator to the list
+##### Store estimator for this model #####
+estimators = []
+for estimator in multi_target_logreg.estimators_:
+    # Append the current estimator to the list
     estimators.append(estimator.coef_)
 
 estimators = np.vstack(estimators).T
-#print(coefficients.shape)
+# print(coefficients.shape)
 coef_path = "../data/pred_data/final/estimators.csv"
 np.savetxt(coef_path, estimators, delimiter=',')
 
-## References: CSC311 Winter2023-2024 lab9
-## Example model initialization function for Logistic Regression
-# def init_log_reg_model():
-#    return LogisticRegression(max_iter=1000)
-#
-#
-## DataFrame clean_data with 'Label' as the target column
-# mean_acc, scores = train_and_evaluate_k_fold(k_folds, init_log_reg_model)
-# print(f"Mean Accuracy: {mean_acc}")
-#
-###### Prediction ####
-# test_pred = model.predict(X_test)
-# print(f"Test Accuracy: {accuracy_score(dc.t_test, test_pred)}")
-# print(f"Precision:{precision_score(dc.t_test, test_pred, average='macro')}")
-#
-##### Store coefficients and intercept for this model #####
-# print("Model intercept:", model.intercept_)
-# intercept = model.intercept_
-## Save intercept to its own text file
-# intercept_path = '/Users/fermis/Desktop/CSC311/CSC311_ML/data/pred_data/intercept.txt'
-# with open(intercept_path, 'w') as file:
-#    file.write(f"Model intercept: {intercept}\n")
-#
-### for final inter
-##intercept = model.intercept_
-### Save intercept to its own text file
-##intercept_path = '/Users/fermis/Desktop/CSC311/CSC311_ML/data/pred_data/final/final_intercept.txt'
-##with open(intercept_path, 'w') as file:
-##    file.write(f"Model intercept: {intercept}\n")
-#
-# coefficients = []
-##print("Model coefficients:")
-# for feature, cof in zip(X_train_df.columns, model.coef_[0]):
-#    print(f"{feature}: {cof}")
-#    coefficients.append(cof)
-#
-# features = X_train_df.columns
-## Combine the feature names and coefficients into a DataFrame
-# coefs_df = pd.DataFrame({
-#    'Feature': features,
-#    'Coefficient': coefficients
-# })
-#
-## for final coef
-##coefficients = []
-###print("Model coefficients:")
-##for feature, cof in zip(X_t_df.columns, model.coef_[0]):
-##    #print(f"{feature}: {cof}")
-##    coefficients.append(cof)
-##
-##features = X_t_df.columns
-### Combine the feature names and coefficients into a DataFrame
-##coefs_df = pd.DataFrame({
-##   'Feature': features,
-##   'Coefficient': coefficients
-##})
-## Save to CSV
-# csv_file_path = '/Users/fermis/Desktop/CSC311/CSC311_ML/data/pred_data/coefficients.csv'
-## Save coefficients to their own CSV file
-# coefs_df.to_csv(csv_file_path, index=False)
-#
-## Save to CSV
-##sv_file_path = '/Users/fermis/Desktop/CSC311/CSC311_ML/data/pred_data/final/final_coefficients.csv'
-## Save coefficients to their own CSV file
-##oefs_df.to_csv(csv_file_path, index=False)
+
+
+#### K-folder cross validation ####
+# Define the cross-validator
+cv = KFold(n_splits=5, random_state=18, shuffle=True)
+
+scores = cross_val_score(multi_target_logreg, X_train, dc.t_train, cv=cv, scoring='accuracy')
+
+print("Cross-validation scores:", scores)
+print("Mean Cross-validation accuracy:", np.mean(scores))
